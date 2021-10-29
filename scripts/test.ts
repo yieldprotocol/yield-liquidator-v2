d38d16a67f5c31b5c7b2aa6db8632ce710cf193b
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-10-29 11:34:20 +0100

    test: better logs

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,7 +1,7 @@
 import { ethers, network } from 'hardhat'
 import * as hre from 'hardhat'
 
-import { FlashLiquidator, Cauldron, Witch } from '../typechain/'
+import { FlashLiquidator, Cauldron, Witch, IERC20 } from '../typechain/'
 
 /**
  * @dev This script tests the FlashLiquidator
@@ -9,10 +9,12 @@ import { FlashLiquidator, Cauldron, Witch } from '../typechain/'
  */
 
 ;(async () => {
+    const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
     const cauldronAddress = '0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867'
     const witchAddress = '0x53C3760670f6091E1eC76B4dd27f73ba4CAd5061'
     const timelockAddress = '0x3b870db67a45611CF4723d44487EAF398fAc51E3'
     const flashLiquidatorAddress = '0x51A1ceB83B83F1985a81C295d1fF28Afef186E02' // Check for each test
+    const recipient = '0x3b43618b2961D5fbDf269A72ACcb225Df70dCb48'
 
     const ETH = ethers.utils.formatBytes32String('00').slice(0, 14)
     const DAI = ethers.utils.formatBytes32String('01').slice(0, 14)
@@ -32,6 +34,7 @@ import { FlashLiquidator, Cauldron, Witch } from '../typechain/'
     ]);
 
     // Contract instantiation
+    const dai = (await ethers.getContractAt('IWETH9', daiAddress, ownerAcc)) as unknown as IERC20
     const cauldron = (await ethers.getContractAt('Cauldron', cauldronAddress, ownerAcc)) as unknown as Cauldron
     const witch = (await ethers.getContractAt('Witch', witchAddress, ownerAcc)) as unknown as Witch
     const flashLiquidator = (await ethers.getContractAt('FlashLiquidator', flashLiquidatorAddress, ownerAcc)) as unknown as FlashLiquidator
@@ -46,14 +49,19 @@ import { FlashLiquidator, Cauldron, Witch } from '../typechain/'
     });
     const timelockAcc = await ethers.getSigner(timelockAddress)
     const oracleAddress = (await cauldron.spotOracles(DAI, ETH)).oracle
+    console.log(`Raising required collateralization to 300%`)
     await cauldron.connect(timelockAcc).setSpotOracle(DAI, ETH, oracleAddress, 3000000)
 
     // Liquidate the vault
+    console.log(`Auctioning ${vaultId}`)
     await witch.auction(vaultId)
 
     // Wait to get enough collateral to pay the flash loan plus the fees
     const { timestamp } = await ethers.provider.getBlock('latest')
     await ethers.provider.send('evm_mine', [timestamp + 3600])
 
+    console.log(`Liquidating ${vaultId}`)
     await flashLiquidator.liquidate(vaultId)
+
+    console.log(`Profit: ${await dai.balanceOf(recipient)}`)
 })()
