aaa285e107b9c7ba153879655477bb1e15a45d8f
iamsahu <praffulsahu@gmail.com>
2022-03-09 18:20:59 +0530

    fix: comments

--- contracts/Cvx3CrvFlashLiquidator.sol
+++ contracts/Cvx3CrvFlashLiquidator.sol
@@ -1,8 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 // 3CRV: https://etherscan.io/address/0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490
 // Convex deposit contract: https://etherscan.io/address/0x689440f2Ff927E1f24c72F1087E1FAF471eCe1c8
 // cvx3CRV: https://etherscan.io/address/0x30d9410ed1d5da1f6c8391af5338c93ab8d4035c
-
-// SPDX-License-Identifier: GPL-2.0-or-later
 
 pragma solidity >=0.8.6;
 
@@ -12,7 +11,7 @@ import './ICurveStableSwap.sol';
 import './IWstEth.sol';
 import './IBaseRewardPool.sol';
 
-// @notice This is the Yield Flash liquidator contract for Lido Wrapped Staked Ether (WSTETH)
+// @notice This is the Flash liquidator contract for CVX3CRV
 contract Cvx3CrvFlashLiquidator is FlashLiquidator {
     using UniswapTransferHelper for address;
     using UniswapTransferHelper for IWstEth;
@@ -36,7 +35,7 @@ contract Cvx3CrvFlashLiquidator is FlashLiquidator {
     // @param amounts amounts of tokens loaned
     // @param feeAmounts flash loan fees
     // @dev        Unlike the other Yield FlashLiquidator contracts, this contains extra steps to
-    //             unwrap WSTETH and swap it for Eth on Curve before Uniswapping it for base
+    //             unwrap cvx3CRV to 3CRV and swap it for DAI/USDC/WEth on Uniswap
     function receiveFlashLoan(
         address[] memory tokens,
         uint256[] memory amounts,
